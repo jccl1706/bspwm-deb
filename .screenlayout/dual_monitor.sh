a0713c6826eb3e84fe1abb2cd97c96c682515e83
jc <jccl1706@gmail.com>
2024-02-21 12:17:32 -0500

    new changes

---
+++ .screenlayout/dual_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 1440x766 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rotate right --output DisplayPort-2 --off --output HDMI-A-0 --off
